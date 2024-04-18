77717e64edf5e4a95958f12daab555aa8fc32557
Zbyněk Vanžura <heptau@gmail.com>
2024-04-18 12:15:19 +0200

    Unify indentation, update chart.js library

--- js/app.js
+++ js/app.js
@@ -18,47 +18,47 @@ const data = [];
 const labels = [];
 
 function calculateGrowth(e) {
-    e.preventDefault();
-    data.length = 0;
-    labels.length = 0;
-    let growth = 0;
-    try {
-        const initial = parseInt(intialAmount.value);
-        const period = parseInt(years.value);
-        const interest = parseInt(rates.value);
-        const comp = parseInt(compound.value);
+	e.preventDefault();
+	data.length = 0;
+	labels.length = 0;
+	let growth = 0;
+	try {
+		const initial = parseInt(intialAmount.value);
+		const period = parseInt(years.value);
+		const interest = parseInt(rates.value);
+		const comp = parseInt(compound.value);
 
-        for(let i = 1; i <= period; i++) {
-            const final = initial * Math.pow(1 + ((interest / 100) / comp), comp * i);
-            data.push(toDecimal(final, 2));
-            labels.push("Year " + i);
-            growth = toDecimal(final, 2);
-        }
-        //
-        message.innerText = `You will have this amount ${growth} after ${period} years`;
-        drawGraph();
-    } catch (error) {
-        console.error(error);
-    }
+		for(let i = 1; i <= period; i++) {
+			const final = initial * Math.pow(1 + ((interest / 100) / comp), comp * i);
+			data.push(toDecimal(final, 2));
+			labels.push("Year " + i);
+			growth = toDecimal(final, 2);
+		}
+		//
+		message.innerText = `You will have this amount ${growth} after ${period} years`;
+		drawGraph();
+	} catch (error) {
+		console.error(error);
+	}
 }
 
 function drawGraph() {
-    line.destroy();
-    line = new Chart(context, {
-        type: 'line',
-        data: {
-            labels,
-            datasets: [{
-                label: "compound",
-                data,
-                fill: true,
-                backgroundColor: "rgba(12, 141, 0, 0.7)",
-                borderWidth: 3
-            }]
-        }
-    });
+	line.destroy();
+	line = new Chart(context, {
+		type: 'line',
+		data: {
+			labels,
+			datasets: [{
+				label: "compound",
+				data,
+				fill: true,
+				backgroundColor: "rgba(12, 141, 0, 0.7)",
+				borderWidth: 3
+			}]
+		}
+	});
 }
 
 function toDecimal(value, decimals) {
-    return +value.toFixed(decimals);
+	return +value.toFixed(decimals);
 }
