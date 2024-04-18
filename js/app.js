31a0dbf9226c28aa790089c5a434a0ce0d3db54e
Zbyněk Vanžura <heptau@gmail.com>
2024-04-18 15:08:10 +0200

    Add localization to Czech language

--- js/app.js
+++ js/app.js
@@ -6,13 +6,19 @@ const years = document.getElementById("years");
 const rates = document.getElementById("rates");
 const compound = document.getElementById("compound");
 
+var lang = "en";
+
 //Messge
 const message = document.getElementById("message");
 
 //The calculate button
-const button = document.querySelector(".input-group button");
-//Attach an event listener
-button.addEventListener("click", calculateGrowth);
+// Find all buttons inside a div with class "input-group"
+const buttons = document.querySelectorAll(".input-group button");
+
+// Add event listeners for each button
+buttons.forEach(button => {
+    button.addEventListener("click", calculateGrowth);
+});
 
 const data = [];
 const labels = [];
@@ -31,11 +37,21 @@ function calculateGrowth(e) {
 		for(let i = 1; i <= period; i++) {
 			const final = initial * Math.pow(1 + ((interest / 100) / comp), comp * i);
 			data.push(toDecimal(final, 2));
-			labels.push("Year " + i);
+			if (lang === "cs") {
+				labels.push("Rok " + i);
+			} else {
+				labels.push("Year " + i);
+			}
+
 			growth = toDecimal(final, 2);
 		}
 		//
-		message.innerText = `You will have this amount ${growth} after ${period} years`;
+		if (lang === "cs") {
+			message.innerText = `Po ${period} letech budete mít ${growth} USD`;
+		} else {
+			message.innerText = `You will have ${growth} USD after ${period} years`;
+		}
+
 		drawGraph();
 	} catch (error) {
 		console.error(error);
@@ -62,3 +78,37 @@ function drawGraph() {
 function toDecimal(value, decimals) {
 	return +value.toFixed(decimals);
 }
+
+function switchLanguage(newLang) {
+	lang = newLang;
+	// Show and hide corresponding elements
+	const elements = document.querySelectorAll('[data-lang]');
+	elements.forEach(element => {
+		 if (element.getAttribute('data-lang') === newLang) {
+			  element.classList.remove('hidden');
+		 } else {
+			  element.classList.add('hidden');
+		 }
+	});
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+	// Find out your preferred browser language
+	const preferredLanguage = navigator.language || navigator.languages[0];
+
+	// Find out the language abbreviation (e.g. "en", "cs")
+	const languageCode = preferredLanguage.substring(0, 2);
+
+	// We find the language selection element
+	const langSelect = document.getElementById("langSelect");
+
+	// Set the selected language based on browser preference
+	if (languageCode === "cs" || languageCode === "sk") {
+		 langSelect.value = "cs"; // Pokud je preferovaný jazyk čeština nebo slovenština, nastavíme češtinu
+	} else {
+		 langSelect.value = "en"; // Jinak nastavíme angličtinu
+	}
+
+	// Call the function to change the language
+	switchLanguage(langSelect.value);
+});
